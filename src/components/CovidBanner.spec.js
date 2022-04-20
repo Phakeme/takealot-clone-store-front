b43f0c2517469dde0c5294e4733aef78d00ca590
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-20 22:02:59 +0200

    src/components/CovidBanner.spec.js

--- src/components/CovidBanner.spec.js
+++
@@ -1,7 +0,0 @@
-import { CovidBanner } from "./CovidBanner";
-import { screen, render } from "@testing-library/react";
-
-it("renders covid banner text info", () => {
-  render(<CovidBanner />);
-  expect(screen.getByText(/Stay informed!/)).toBeInTheDocument();
-});
