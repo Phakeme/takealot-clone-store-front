e7cb12e39f7944194737468ccd5459494f2c92ff
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-20 22:04:21 +0200

    src/components/mainHeader/MainHeader.jsx

---
+++ src/components/mainHeader/MainHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MainHeader = () => {
+  return (
+    <header className="">
+      <div className="bg-white h-16">Codealot</div>
+      <div className="bg-blue h-20"></div>
+    </header>
+  );
+};
