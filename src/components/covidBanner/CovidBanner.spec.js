44227bd8c373c94d552d6903a96de010d8915c11
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-20 22:03:51 +0200

    src/components/covidBanner/CovidBanner.spec.js

---
+++ src/components/covidBanner/CovidBanner.spec.js
@@ -0,0 +1,21 @@
+import { CovidBanner } from "./CovidBanner";
+import { screen, render } from "@testing-library/react";
+
+it("renders Stay informed!", () => {
+  render(<CovidBanner />);
+  expect(screen.getByText(/Stay informed!/)).toBeInTheDocument();
+});
+
+it("renders Visit the SA Department of Health's website for COVID-19 updates:", () => {
+  render(<CovidBanner />);
+  expect(
+    screen.getByText(
+      /Visit the SA Department of Health's website for COVID-19 updates:/
+    )
+  ).toBeInTheDocument();
+});
+
+it("renders www.sacoronavirus.co.za", () => {
+  render(<CovidBanner />);
+  expect(screen.getByText(/www.sacoronavirus.co.za/)).toBeInTheDocument();
+});
