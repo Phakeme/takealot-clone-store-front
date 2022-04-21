6c0207e3189dd3a93a4bdabf950ceb4b0c83cc62
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-21 23:13:29 +0200

    src/components/mainHeader/MainHeader.jsx

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import brandLogo from "../../images/takealot-logo.svg";
+import { Link } from "react-router-dom";
+import { NavLink } from "./NavLink";
 
 export const MainHeader = () => {
   return (
-    <header className="">
-      <div className="bg-white h-16">Codealot</div>
+    <header>
+      <div className="bg-white h-16">
+        <div className="h-full container mx-auto flex justify-between items-center">
+          <div className="flex">
+            <Link to="/">
+              <img
+                className="mr-6"
+                width="180px"
+                src={brandLogo}
+                alt="codealot-brand"
+              />
+            </Link>
+            <div className="flex items-center divide-x">
+              <NavLink path="/help" label="Help" />
+              <NavLink path="/sell" label="Sell on Takealot" />
+            </div>
+          </div>
+          <div className="flex items-center divide-x">
+            <NavLink path="/login" label="Login" />
+            <NavLink path="/register" label="Register" />
+            <NavLink path="/orders" label="Orders" />
+            <NavLink path="/my-account" label="My Account" />
+            <NavLink path="/chart" label="Cart & Wish" />
+          </div>
+        </div>
+      </div>
       <div className="bg-blue h-20"></div>
     </header>
   );
