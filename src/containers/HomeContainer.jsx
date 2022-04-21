4a3fc92340e2de6aea8418f4608c772761265137
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-21 23:14:49 +0200

    src/containers/HomeContainer.jsx

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import { CovidBanner } from "../components/covidBanner/CovidBanner";
-import { MainHeader } from "../components/mainHeader/MainHeader";
 
 export const HomeContainer = () => {
-  return (
-    <>
-      <CovidBanner />
-      <MainHeader />
-    </>
-  );
+  return <div className="container mx-auto">Home</div>;
 };
