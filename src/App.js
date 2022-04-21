d67c267ae88c06195bf69a4a2796902a9b199506
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-21 23:13:09 +0200

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import { HomeContainer } from "./containers/HomeContainer";
+import { HelpContainer } from "./containers/HelpContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CovidBanner } from "./components/covidBanner/CovidBanner";
+import { MainHeader } from "./components/mainHeader/MainHeader";
 
 function App() {
-  return <HomeContainer />;
+  return (
+    <div className="text-gray-700">
+      <BrowserRouter>
+        <CovidBanner />
+        <MainHeader />
+        <Routes>
+          <Route exact path="/" element={<HomeContainer />}></Route>
+          <Route exact path="/help" element={<HelpContainer />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
