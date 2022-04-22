0b330c52950bd5267a2e8c787e95b56cc687d3c1
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-22 21:58:53 +0200

    src/components/mainHeader/NavLink.jsx

--- src/components/mainHeader/NavLink.jsx
+++ src/components/mainHeader/NavLink.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const NavLink = ({ path, label }) => {
   return (
     <Link to={path}>
-      <div className="py-2 px-3 text-sm hover:text-blue hover:underline">
+      <div className="py-2 px-4 text-sm hover:text-blue hover:underline">
         {label}
       </div>
     </Link>
