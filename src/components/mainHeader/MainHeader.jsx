ac16115170a9d434b6236c99c93bec3527b6990a
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-22 21:59:21 +0200

    src/components/mainHeader/MainHeader.jsx

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import brandLogo from "../../images/takealot-logo.svg";
+import Heart from "../../images/heart.svg";
+import Cart from "../../images/shopping-cart.svg";
+import ArrowDown from "../../images/arrow-down.svg";
 import { Link } from "react-router-dom";
 import { NavLink } from "./NavLink";
 
@@ -26,8 +29,25 @@ export const MainHeader = () => {
             <NavLink path="/login" label="Login" />
             <NavLink path="/register" label="Register" />
             <NavLink path="/orders" label="Orders" />
-            <NavLink path="/my-account" label="My Account" />
-            <NavLink path="/chart" label="Cart & Wish" />
+
+            <div className="text-sm flex items-center justify-center  pl-2 h-8s">
+              <div className="flex items-center justify-center p-2 cursor-pointer">
+                <div>My Account</div>
+                <div className="pl-2 ">
+                  <img width="8px" src={ArrowDown} alt="" />
+                </div>
+              </div>
+              <div className="flex items-center justify-center mx-2 rounded-full bg-red-400 p-2 radius-2 cursor-pointer">
+                <img width="17px" src={Heart} alt="" />
+              </div>
+
+              <div className="h-full flex items-center justify-center bg-green-700 rounded-full p-2 text-white cursor-pointer">
+                <div className="">
+                  <img width="18px" src={Cart} alt="" />
+                </div>
+                <div className="pl-2 pr-1">0</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
