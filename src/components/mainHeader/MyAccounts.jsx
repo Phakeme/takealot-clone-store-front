73663963ca8bb4aaf14257ae3dea314b7a684f5a
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-24 23:23:07 +0200

    src/components/mainHeader/MyAccounts.jsx

--- src/components/mainHeader/MyAccounts.jsx
+++ src/components/mainHeader/MyAccounts.jsx
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { NavLink } from "./NavLink";
 
 import ArrowDown from "../../images/arrow-down.svg";
 import ArrowUp from "../../images/arrow-up.svg";
 
 export const MyAccounts = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const data = [
+    { label: "My Account", path: "#" },
+    { label: "Track Order", path: "#" },
+    { label: "Returns", path: "#" },
+    { label: "Credit & Refunds", path: "#" },
+    { label: "Products Reviews", path: "#" },
+    { label: "Invoices", path: "#" },
+    { label: "Personal Detailes", path: "#" },
+    { label: "Digital Libary", path: "#" },
+    { label: "Help", path: "/help" },
+  ];
   const DropDownMenu = () => {
     return (
-      <div className="absolute bg-white top-8 right-1 w-40 p-4 drop-shadow-md">
+      <div className="absolute bg-white top-8 right-1 w-40 py-1 drop-shadow-md">
         <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 2</li>
-          <li>Item 2</li>
-          <li>Item 2</li>
+          {data.map(({ label, path }, index) => (
+            <li key={index}>
+              <NavLink label={label} path={path} />
+            </li>
+          ))}
         </ul>
       </div>
     );
@@ -24,9 +37,16 @@ export const MyAccounts = () => {
       onMouseLeave={() => setIsOpen(false)}
       className="flex items-center justify-center p-2 cursor-pointer relative"
     >
-      <div>My Account</div>
+      <Link to="/account">
+        <div>My Account</div>
+      </Link>
       <div className="pl-2 ">
-        <img width="8px" src={isOpen ? ArrowUp : ArrowDown} alt="My Account" />
+        <img
+          width="8px"
+          height="auto"
+          src={isOpen ? ArrowUp : ArrowDown}
+          alt="My Account"
+        />
       </div>
       {isOpen && <DropDownMenu />}
     </div>
