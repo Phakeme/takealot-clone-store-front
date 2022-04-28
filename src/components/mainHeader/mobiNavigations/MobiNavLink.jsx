41717eff8a82af22dfe7d28043b99a9e562269b3
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-28 19:28:55 +0200

    src/components/mainHeader/mobiNavigations/MobiNavLink.jsx

--- src/components/mainHeader/mobiNavigations/MobiNavLink.jsx
+++ src/components/mainHeader/mobiNavigations/MobiNavLink.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const MobiNavLink = ({ path, label }) => {
+export const MobiNavLink = ({ path, label, icon = null, handleCloseSide }) => {
+  const cart = label === "Cart";
+
   return (
     <Link to={path}>
-      <div className="pl-6 bg-white border-b border-gray-200 h-12 flex items-center text-sm">
-        {label}
+      <div
+        onClick={() => handleCloseSide()}
+        className="pl-6 bg-white border-b border-gray-200 h-12 flex items-center text-sm"
+      >
+        {!icon ? (
+          label
+        ) : (
+          <div className="flex items-center justify-between w-full pr-6">
+            <div className="flex items-center ">
+              {icon ? (
+                <div className="mr-5">
+                  <img width="18px" src={icon} alt="" />
+                </div>
+              ) : null}
+              <div>{label}</div>
+            </div>
+            {cart && <div>0 items</div>}
+          </div>
+        )}
       </div>
     </Link>
   );
