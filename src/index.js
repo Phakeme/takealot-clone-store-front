3beb2c37f3fd773d3907d31b4604f44250b6155c
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-30 03:30:02 +0200

    src/index.js0

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./data";
+import store from "./app/app";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
