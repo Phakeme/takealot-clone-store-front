050befc565f8b93dd43add91fc5b1f2accdcfdbf
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-04-30 03:33:23 +0200

    src/components/covidBanner/CovidBanner.spec.js

--- src/components/covidBanner/CovidBanner.spec.js
+++ src/components/covidBanner/CovidBanner.spec.js
@@ -1,21 +1,23 @@
 import { CovidBanner } from "./CovidBanner";
 import { screen, render } from "@testing-library/react";
 
-it("renders Stay informed!", () => {
-  render(<CovidBanner />);
-  expect(screen.getByText(/Stay informed!/)).toBeInTheDocument();
-});
+describe("covid banner renders text correctly", () => {
+  it("renders Stay informed!", () => {
+    render(<CovidBanner />);
+    expect(screen.getByText(/Stay informed!/)).toBeInTheDocument();
+  });
 
-it("renders Visit the SA Department of Health's website for COVID-19 updates:", () => {
-  render(<CovidBanner />);
-  expect(
-    screen.getByText(
-      /Visit the SA Department of Health's website for COVID-19 updates:/
-    )
-  ).toBeInTheDocument();
-});
+  it("renders Visit the SA Department of Health's website for COVID-19 updates:", () => {
+    render(<CovidBanner />);
+    expect(
+      screen.getByText(
+        /Visit the SA Department of Health's website for COVID-19 updates:/
+      )
+    ).toBeInTheDocument();
+  });
 
-it("renders www.sacoronavirus.co.za", () => {
-  render(<CovidBanner />);
-  expect(screen.getByText(/www.sacoronavirus.co.za/)).toBeInTheDocument();
+  it("renders www.sacoronavirus.co.za", () => {
+    render(<CovidBanner />);
+    expect(screen.getByText(/www.sacoronavirus.co.za/)).toBeInTheDocument();
+  });
 });
