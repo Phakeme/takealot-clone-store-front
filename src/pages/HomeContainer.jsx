f46e3ce7ad9ef9d5bb6b938c8663ac5e67827b0c
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-02 23:16:38 +0200

    src/pages/HomeContainer.jsx

--- src/pages/HomeContainer.jsx
+++ src/pages/HomeContainer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export const HomeContainer = () => (
-  <div className="container mx-auto">Home</div>
+  <div className="container mx-auto">
+    <div className="bg-red-100 w-full">Home</div>
+  </div>
 );
