28a839197a70bdd4d3dade48e973aefd7a2a7728
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-06 23:32:37 +0200

    src/components/CheckOrderBanner/CheckOrderBanner.jsx

---
+++ src/components/CheckOrderBanner/CheckOrderBanner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Truck from "../../images/truck.svg";
+
+export const CheckOrderBanner = () => {
+  return (
+    <Link to="/test">
+      <div className="flex w-full h-[73px] shadow bg-sky-50 border-2 border-white overflow-hidden">
+        <div className="w-32 relative mr-2 bg-left flex items-center -left-10">
+          <div className="bg-sky-300 rounded-full absolute w-36 h-36 flex items-center">
+            <img className="ml-10" src={Truck} alt="Truck" />
+          </div>
+        </div>
+        <div className="w-3/4 my-auto">
+          <p className="font-semibold mt-0">Where's my order?</p>
+          <p className="text-xs">Check your delivery or collection status.</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
