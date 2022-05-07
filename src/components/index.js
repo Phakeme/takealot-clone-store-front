cfc5b7968bb4309bb593f76a0be7752d60cc8de8
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-07 16:36:06 +0200

    src/components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,13 @@ import { CovidBanner } from "./covidBanner/CovidBanner";
 import { MainHeader } from "./mainHeader/MainHeader";
 import { CheckOrderBanner } from "./CheckOrderBanner/CheckOrderBanner";
 import { SideAdverts } from "./SideAdverts/SideAdverts";
-export { CovidBanner, MainHeader, CheckOrderBanner, SideAdverts };
+import { MainCarousel } from "./MainCarousel/MainCarousel";
+import { LogoCarousel } from "./LogoCarousel/LogoCarousel";
+export {
+  CovidBanner,
+  MainHeader,
+  CheckOrderBanner,
+  SideAdverts,
+  MainCarousel,
+  LogoCarousel,
+};
