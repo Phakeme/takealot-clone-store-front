1da120e3547f9fafbf451f1ac526ab594080491e
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-07 19:26:04 +0200

    src/App.spec.js

---
+++ src/App.spec.js
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import store from "./app/app";
+import { Provider } from "react-redux";
+
+describe("Routing", () => {
+  const setup = (path) => {
+    window.history.pushState({}, "", path);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  };
+  it.each`
+    path       | pageTestId
+    ${"/"}     | ${"home-page"}
+    ${"/help"} | ${"help-page"}
+  `("displays $pageTestId when path is $path", ({ path, pageTestId }) => {
+    setup(path);
+    const page = screen.queryByTestId(pageTestId);
+    expect(page).toBeInTheDocument();
+  });
+
+  it.each`
+    path       | pageTestId
+    ${"/"}     | ${"help-page"}
+    ${"/help"} | ${"home-page"}
+  `(
+    "does not displays $pageTestId when path is $path",
+    ({ path, pageTestId }) => {
+      setup(path);
+      const page = screen.queryByTestId(pageTestId);
+      expect(page).not.toBeInTheDocument();
+    }
+  );
+});
