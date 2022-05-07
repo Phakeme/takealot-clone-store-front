be152c58df98fb1a3b6b45b1346d1c0ae7da153e
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-07 16:37:38 +0200

    src/pages/HomeContainer.jsx

--- src/pages/HomeContainer.jsx
+++ src/pages/HomeContainer.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import { CheckOrderBanner, SideAdverts } from "../components";
+import {
+  CheckOrderBanner,
+  SideAdverts,
+  MainCarousel,
+  LogoCarousel,
+} from "../components";
 
 export const HomeContainer = () => (
   <>
     <section className="container mx-auto">
       <div className="grid grid-cols-[198px,1fr,300px] gap-6 h-[440px] mt-5">
         <div />
-        <div className="bg-red-100">hero</div>
+        <div className="">
+          <MainCarousel />
+          <div className="flex flex-col">
+            <h3 className="py-4 font-semibold">
+              See My Other Portfolio Projects
+            </h3>
+            <LogoCarousel />
+          </div>
+        </div>
         <div className="w-full relative">
           <CheckOrderBanner />
           <SideAdverts />
         </div>
       </div>
     </section>
-    <div className="bg-gray-100 h-72">text</div>
+    <div className="bg-gray-100 h-72">
+      {/* <LogoCarousel /> */}
+      test
+    </div>
   </>
 );
