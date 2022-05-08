c7f1d6d941c122669d36637b54ecea43e90ec830
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-08 09:48:06 +0200

    Refactoring Folders and Files

---
+++ src/components/mainHeader/CategoryTopNavSingleLink.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const CategoryTopNavSingleLink = ({ label, path }) => {
+  return (
+    <li className="h-full hover:bg-gray-200 grow">
+      <Link to={path}>
+        <div className="flex items-center justify-center h-full">{label}</div>
+      </Link>
+    </li>
+  );
+};
