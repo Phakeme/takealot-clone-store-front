4d29a3083104468ee7b0d6f2fa6dab4e89c787c4
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-08 09:48:57 +0200

    Refactoring Folders and Files

---
+++ src/components/mainHeader/PagesTopNavSingleLink.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const PagesTopNavSingleLink = ({ path, label, key }) => {
+  return (
+    <Link to={path} key={key}>
+      <div className="py-1 px-4 text-sm hover:text-blue hover:underline">
+        {label}
+      </div>
+    </Link>
+  );
+};
