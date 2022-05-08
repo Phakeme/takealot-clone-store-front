fef8cd8bf0d016dfc6bb8a8e94b5239cac018b56
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-08 09:48:41 +0200

    Refactoring Folders and Files

---
+++ src/components/mainHeader/SideBarCategorySingleLink.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import rightIcon from "../../images/right-arrow.svg";
+
+export const SideBarCategorySingleLink = ({ label, path }) => {
+  return (
+    <Link key={label} to={path}>
+      <li className="bg-white w-full h-6 hover: hover:bg-blue hover:text-white  items-center px-3 flex justify-between ">
+        <span>{label}</span>
+        <span className="fill-white">
+          <img width="8px" src={rightIcon} alt="" />
+        </span>
+      </li>
+    </Link>
+  );
+};
