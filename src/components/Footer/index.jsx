296d118a6d0457ed8bbf03a0a313d51afe974993
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-15 16:01:20 +0200

    src/components/Footer/index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div className="bg-white h-[200px]">Footer</div>;
+  return (
+    <div className="bg-white h-[200px]">
+      <div className="container mx-auto">
+        <details>
+          <summary>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam
+            qui ullam ea.
+          </summary>
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            sapiente eos repudiandae molestias, cupiditate expedita officia ab
+            perspiciatis delectus at dolores minima? Consequatur dolorum esse
+            accusamus natus pariatur quo officiis.
+          </div>
+        </details>
+      </div>
+    </div>
+  );
 };
