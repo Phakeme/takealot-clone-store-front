cedb34e6ec3c2c351f13b15ba1a4f027deec8475
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-16 21:30:44 +0200

    src/components/Footer/FooterModule.jsx

---
+++ src/components/Footer/FooterModule.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const FooterModule = ({ footerModule }) => {
+  return (
+    <div>
+      <h3 className="font-bold mb-2">{footerModule.title}</h3>
+      <ul>
+        {footerModule?.footerLinks?.map((item, index) => (
+          <li
+            key={index}
+            className="text-sm hover:text-blue w-fit hover:underline"
+          >
+            <Link to="/deals">{item.textlabel}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
