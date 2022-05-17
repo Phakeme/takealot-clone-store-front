16c3ea7ad86887a93eeadaa3219aa4da36dac6ed
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-17 23:07:23 +0200

    src/components/Footer/data.js

---
+++ src/components/Footer/data.js
@@ -0,0 +1,60 @@
+// import Mastercard from "../../images/footer/mastercard.svg";
+// import Diners from "../../images/footer/diners.svg";
+// import Ebucks from "../../images/footer/ebucks.svg";
+// import Mobi from "../../images/footer/mobi.svg";
+import Payfast from "../../images/footer/payfast-cropped.svg";
+import Ozow from "../../images/footer/ozow.svg";
+import Express from "../../images/footer/express.svg";
+import Visa from "../../images/footer/visa.svg";
+
+export const footerModulesData = [
+  {
+    title: "Shop",
+    footerLinks: [
+      { textlabel: "Daily Deals", path: "/" },
+      { textlabel: "App Only Deals", path: "/" },
+      { textlabel: "Clearance Sale", path: "/" },
+      { textlabel: "Gift Vouchers", path: "/" },
+    ],
+  },
+  {
+    title: "Help",
+    footerLinks: [
+      { textlabel: "Help", path: "/" },
+      { textlabel: "Contact Us", path: "/" },
+      { textlabel: "Submit an Idea", path: "/" },
+      { textlabel: "Suggest a Product", path: "/" },
+      { textlabel: "Shipping & Delivery", path: "/" },
+      { textlabel: "Takealot Pickup Points", path: "/" },
+      { textlabel: "Returns", path: "/" },
+    ],
+  },
+  {
+    title: "Account",
+    footerLinks: [
+      { textlabel: "My Account", path: "/" },
+      { textlabel: "Track Order", path: "/" },
+      { textlabel: "Returns", path: "/" },
+      { textlabel: "Personal Details", path: "/" },
+      { textlabel: "Invoices", path: "/" },
+      { textlabel: "Digital Library", path: "/" },
+    ],
+  },
+  {
+    title: "Company",
+    footerLinks: [
+      { textlabel: "About Us", path: "/" },
+      { textlabel: "Careers", path: "/" },
+      { textlabel: "Sell on Takealot", path: "/" },
+      { textlabel: "Deliver for Takealot", path: "/" },
+      { textlabel: "Press & News", path: "/" },
+      { textlabel: "Terms & Conditions", path: "/" },
+      { textlabel: "Competitions", path: "/" },
+      { textlabel: "Privacy Policy", path: "/" },
+      { textlabel: "Human Rights Statement", path: "/" },
+      { textlabel: "Speak Up Process", path: "/" },
+    ],
+  },
+];
+
+export const footerLogosData = [Visa, Ozow, Payfast, Express];
