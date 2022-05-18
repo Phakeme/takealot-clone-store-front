3867bb8ac596dae40d249f76a1cc3a443663ed8d
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-18 23:07:03 +0200

    src/pages/ProductContainer.jsx

--- src/pages/ProductContainer.jsx
+++ src/pages/ProductContainer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const ProductContainer = () => {
-  return <div className="h-[700px] bg-red-100">ProductContainer</div>;
+  return (
+    <section className="h-[700px] bg-gray-100">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-[1fr_325px] pt-6 gap-6">
+          <div className="bg-white border border-gray-200 p-6">
+            ProductContainer
+          </div>
+          <div className="bg-white h-[260px] border border-gray-200 p-6">
+            ProductContainer
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
