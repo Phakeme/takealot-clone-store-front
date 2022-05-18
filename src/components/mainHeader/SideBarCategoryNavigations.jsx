60efb635531a17e77f8f42258d04eff29d26d537
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-18 23:08:43 +0200

    src/components/mainHeader/SideBarCategoryNavigations.jsx

--- src/components/mainHeader/SideBarCategoryNavigations.jsx
+++ src/components/mainHeader/SideBarCategoryNavigations.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import TimeWhiteIcon from "../../images/time-white-icon.svg";
 import downArrowWhite from "../../images/down-arrow-white.svg";
 import { SideBarCategorySingleLink } from "./SideBarCategorySingleLink";
 
 export const SideBarCategoryNavigations = ({ heroNavigations }) => {
+  const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setIsOpen(true);
+    } else {
+      setIsOpen(false);
+    }
+  }, [location]);
+
+  const handleMouseOut = () => {
+    if (location.pathname === "/") {
+      return;
+    }
+    setIsOpen(false);
+  };
   return (
-    <div className="relative hidden lg:block">
+    <div
+      className="relative hidden lg:block"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => handleMouseOut()}
+    >
       <div className=" absolute w-full shadow-lg bg-white text-sm">
         <div className="bg-gray-700 w-full h-8 text-white flex items-center px-3 text-xs justify-between cursor-pointer">
           <span>Shop by Department</span>
@@ -13,13 +35,16 @@ export const SideBarCategoryNavigations = ({ heroNavigations }) => {
             <img width="8px" src={downArrowWhite} alt="" />
           </span>
         </div>
-        <ul className="my-2">
-          {heroNavigations?.heroNavLinks?.map(({ label, path }) => (
-            <div key={path}>
-              <SideBarCategorySingleLink label={label} path={path} />
-            </div>
-          ))}
-        </ul>
+        {isOpen && (
+          <ul className="my-2">
+            {heroNavigations?.heroNavLinks?.map(({ label, path }) => (
+              <div key={path}>
+                <SideBarCategorySingleLink label={label} path={path} />
+              </div>
+            ))}
+          </ul>
+        )}
+
         <div className="bg-green-500 w-full h-8 text-white flex items-center justify-center px-3 text-lg tracking-wide cursor-pointer">
           <span className="mr-2">
             <img width="13px" src={TimeWhiteIcon} alt="" />
