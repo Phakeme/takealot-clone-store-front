fa21753c4165f881ba5f212bf2c3127f249e561c
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-18 21:25:59 +0200

    src/components/Utils/LoaderSpinner/index.jsx

---
+++ src/components/Utils/LoaderSpinner/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export const LoaderSpinner = () => {
+  return (
+    <div class="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
