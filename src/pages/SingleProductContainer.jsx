03274330debdfabe17a989bde15323838f408c31
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-20 20:59:38 +0200

    src/pages/SingleProductContainer.jsx

--- src/pages/SingleProductContainer.jsx
+++ src/pages/SingleProductContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useChecResultContext } from "../Context/ChecContextProvider";
+import { PriceDisplay, MainProductDisplay } from "../components";
 
 export const SingleProductContainer = () => {
   const { singleProduct, getSingleProduct, isSingleProductLoading } =
@@ -12,34 +13,11 @@ export const SingleProductContainer = () => {
   }, []);
 
   return (
-    <section className="h-[700px] bg-gray-100">
+    <section className="bg-gray-100">
       <div className="container mx-auto">
-        <div className="grid grid-cols-[1fr_325px] pt-6 gap-6">
-          <div className="bg-white border border-gray-200 p-6">
-            {singleProduct?.name}
-          </div>
-          <div className="bg-white h-[260px] border border-gray-200 px-6 pt-4">
-            <div className="">
-              <h2 className="text-[35px] font-bold mb-0">
-                {singleProduct?.price?.formatted_with_symbol}
-              </h2>
-              <div className="uppercase relative font-bold mb-8">
-                <span className="text-xs absolute -top-1">Free Delivery</span>
-              </div>
-              <div className="text-xs mb-2">
-                <p>eB209,990 Discovery Miles 209,990</p>
-                <p>On Credit: R1,288 / month</p>
-              </div>
-              <div className="">
-                <div className="bg-green-700 h-[39px] text-white mb-3 flex justify-center items-center">
-                  Add to Cart
-                </div>
-                <div className="bg-gray-100 h-[39px] flex justify-center items-center">
-                  Add to List
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="grid md:grid-cols-[380px_1fr] lg:grid-cols-[1fr_325px] py-6 gap-6">
+          <MainProductDisplay singleProduct={singleProduct} />
+          <PriceDisplay singleProduct={singleProduct} />
         </div>
       </div>
     </section>
