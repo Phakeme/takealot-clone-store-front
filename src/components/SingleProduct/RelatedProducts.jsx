cfd7d2b11dd0355297cd0d460f5f5e8db195d9de
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-21 20:01:08 +0200

    src/components/SingleProduct/RelatedProducts.jsx

---
+++ src/components/SingleProduct/RelatedProducts.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useChecResultContext } from "../../Context/ChecContextProvider";
+import { ProductCard } from "../Products/ProductCard";
+
+export const RelatedProducts = ({ singleProduct }) => {
+  const { getSingleProduct } = useChecResultContext();
+  return (
+    <div className="mt-6 hidden md:block">
+      <h2 className="font-bold mb-3">Related Products</h2>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6">
+        {singleProduct?.related_products?.slice(0, 2).map((item) => (
+          <div key={item.id} onClick={() => getSingleProduct(item.id)}>
+            <div className="h-full">
+              <ProductCard product={item} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
