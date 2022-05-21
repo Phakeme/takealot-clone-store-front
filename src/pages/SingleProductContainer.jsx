fa8d3654717a6314e0382fe10e5d49cb3c853016
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-21 19:59:03 +0200

    src/pages/SingleProductContainer.jsx

--- src/pages/SingleProductContainer.jsx
+++ src/pages/SingleProductContainer.jsx
@@ -4,8 +4,8 @@ import { useChecResultContext } from "../Context/ChecContextProvider";
 import { PriceDisplay, MainProductDisplay } from "../components";
 
 export const SingleProductContainer = () => {
-  const { singleProduct, getSingleProduct, isSingleProductLoading } =
-    useChecResultContext();
+  const { singleProduct, getSingleProduct } = useChecResultContext();
+
   let { productId } = useParams();
   useEffect(() => {
     getSingleProduct(productId);
@@ -15,7 +15,7 @@ export const SingleProductContainer = () => {
   return (
     <section className="bg-gray-100">
       <div className="container mx-auto">
-        <div className="grid md:grid-cols-[380px_1fr] lg:grid-cols-[1fr_325px] py-6 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-[380px_1fr] lg:grid-cols-[1fr_325px] py-6 gap-6">
           <MainProductDisplay singleProduct={singleProduct} />
           <PriceDisplay singleProduct={singleProduct} />
         </div>
