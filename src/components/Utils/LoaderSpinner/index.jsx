1b3bcabac80f2d0f264988f31f7b40a4dda7b140
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-22 09:56:38 +0200

    src/components/Utils/LoaderSpinner/index.jsx

--- src/components/Utils/LoaderSpinner/index.jsx
+++ src/components/Utils/LoaderSpinner/index.jsx
@@ -3,10 +3,14 @@ import "./style.css";
 
 export const LoaderSpinner = () => {
   return (
-    <div className="lds-ring">
-      <div></div>
-      <div></div>
-      <div></div>
+    <div>
+      <div className="flex justify-center items-center  pt-7 lg:pt-2 h-[300px] lg:h-[700px]">
+        <div className="lds-ring ">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
