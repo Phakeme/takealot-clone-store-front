dbae18f62cd1510acf466b779a2777acd6f742b4
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-23 23:24:09 +0200

    src/components/Cart/CartProduct/CartAction.jsx

---
+++ src/components/Cart/CartProduct/CartAction.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const CartAction = ({ icon, textLabel }) => {
+  return (
+    <div className="flex px-2 py-1 rounded hover:cursor-pointer hover:bg-gray-100 ml-0 sm:ml-3 mr-3 sm:mr-0 border sm:border-none">
+      <div className="ms:mr-3 mr-2">
+        <img className="w-[18px]" src={icon} alt="icon" />
+      </div>
+      <span>{textLabel}</span>
+    </div>
+  );
+};
