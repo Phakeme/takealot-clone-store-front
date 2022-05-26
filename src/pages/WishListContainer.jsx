a43c8767760b929fc26f2eadac746e71319e6ded
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-26 21:47:10 +0200

    src/pages/WishListContainer.jsx

---
+++ src/pages/WishListContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const WishListContainer = () => {
+  return (
+    <section className="bg-gray-100">
+      <div className="container mx-auto h-[200px]">WishListContainer</div>
+    </section>
+  );
+};
