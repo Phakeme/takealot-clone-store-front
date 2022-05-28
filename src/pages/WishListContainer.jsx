f8247ef0d7f3a8a1e8b6eaaa862ae2391a40cbab
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-28 08:50:00 +0200

    src/pages/WishListContainer.jsx

--- src/pages/WishListContainer.jsx
+++ src/pages/WishListContainer.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import {
+  CreateWishList,
+  PageTitle,
+  Wishlist,
+  CartProduct,
+} from "../components";
 
 export const WishListContainer = () => {
+  const wishlistProducts = JSON.parse(localStorage.getItem("wishList"));
   return (
-    <section className="bg-gray-100">
-      <div className="container mx-auto h-[200px]">WishListContainer</div>
+    <section className="bg-gray-100 py-6">
+      <div className="container mx-auto">
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-[325px_1fr]">
+          <CreateWishList />
+          <div>
+            <PageTitle titeLabel="Wish List" />
+            <Wishlist>
+              {wishlistProducts?.map((product) => (
+                <CartProduct key={product?.id} product={product} />
+              ))}
+            </Wishlist>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
