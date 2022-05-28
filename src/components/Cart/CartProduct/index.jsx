2961844cf2500ec6e1ff9aba88c7b849841742a7
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-28 08:52:31 +0200

    src/components/Cart/CartProduct/index.jsx

--- src/components/Cart/CartProduct/index.jsx
+++ src/components/Cart/CartProduct/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { StocksLocations } from "../../Utils/StocksLocations";
-import { LoadingSpinner } from "../../Utils/LoadingSpinner";
 import HeartIcon from "../../../images/Cart/gray-heart.svg";
 import BinIcon from "../../../images/Cart/trash.svg";
 import { CartAction } from "./CartAction";
+import { Button } from "../../Utils/Button";
+import CartIcon from "../../../images/shopping-cart.svg";
 import { useChecResultContext } from "../../../Context/ChecContextProvider";
 
 export const CartProduct = ({ product }) => {
-  const { removeFromCart, moveWishList, updateCart, isCartLoading } =
-    useChecResultContext();
+  const location = useLocation();
+
+  const isCartURL = location.pathname === "/cart";
+
+  const { removeFromCart, moveWishList, updateCart } = useChecResultContext();
 
   const handleChange = (e) => {
     updateCart(product.id, e.target.value);
@@ -21,41 +26,37 @@ export const CartProduct = ({ product }) => {
 
   return (
     <div className="h-fit lg:h-[191px] bg-white p-6 border border-gray-200  rounded mb-3 md:mb-6">
-      {isCartLoading ? (
-        <div className="h-full w-full flex justify-center items-center">
-          <LoadingSpinner />
+      <div className="grid grid-cols-1 sm:grid-cols-[100px_1fr] lg:grid-cols-[140px_1fr] gap-6 h-full">
+        <div className="h-full flex justify-center items-center p-2 border-2 sm:border-none">
+          <img
+            className="w-full h-auto"
+            src={product?.image?.url}
+            alt="cart-product"
+          />
         </div>
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-[100px_1fr] lg:grid-cols-[140px_1fr] gap-6 h-full">
-          <div className="h-full flex justify-center items-center p-2 border-2 sm:border-none">
-            <img
-              className="w-full h-auto"
-              src={product?.image?.url}
-              alt="cart-product"
-            />
-          </div>
-          <div className="flex flex-col justify-between">
-            <div className="flex justify-between">
+        <div className="flex flex-col justify-between">
+          <div className="flex justify-between">
+            <div>
+              <h2 className="hidden lg:block">
+                {product?.name.slice(0, 60)}...
+              </h2>
+              <h2 className="block lg:hidden">
+                {product?.name.slice(0, 30)}...
+              </h2>
+              <span className="text-xs text-gray-400">
+                Promotion Applied: Massive Sale
+              </span>
               <div>
-                <h2 className="hidden lg:block">
-                  {product?.name.slice(0, 60)}...
-                </h2>
-                <h2 className="block lg:hidden">
-                  {product?.name.slice(0, 30)}...
-                </h2>
-                <span className="text-xs text-gray-400">
-                  Promotion Applied: Massive Sale
-                </span>
-                <div>
-                  <StocksLocations />
-                </div>
+                <StocksLocations />
               </div>
-              <div className="">
-                <div className="text-[18px] text-right font-bold mb-3">
-                  <span>
-                    {product?.line_total?.formatted_with_symbol?.split(".")[0]}
-                  </span>
-                </div>
+            </div>
+            <div className="">
+              <div className="text-[18px] text-right font-bold mb-3">
+                <span>
+                  {product?.line_total?.formatted_with_symbol?.split(".")[0]}
+                </span>
+              </div>
+              {isCartURL && (
                 <div className="flex">
                   <div className="mr-3 text-sm">Qty:</div>
                   <div>
@@ -82,26 +83,47 @@ export const CartProduct = ({ product }) => {
                     </select>
                   </div>
                 </div>
-              </div>
+              )}
             </div>
-            <div className="flex justify-between">
-              <div className="flex justify-between w-full items-end">
-                <div className="text-xs bg-gray-100 border-2 px-2 py-1 rounded w-fit h-fit border-dashed hidden sm:block">
-                  Eligible for Cash on Delivery
-                </div>
-                <div className="flex text-sm items-center">
-                  <button onClick={() => removeFromCart(product?.id)}>
-                    <CartAction textLabel="Remove" icon={BinIcon} />
-                  </button>
-                  <button onClick={() => handleAddToWishList(product)}>
-                    <CartAction textLabel="Move to list" icon={HeartIcon} />
-                  </button>
-                </div>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex justify-between w-full items-end">
+              <div className="text-xs bg-gray-100 border-2 px-2 py-1 rounded w-fit h-fit border-dashed hidden sm:block">
+                Eligible for Cash on Delivery
+              </div>
+              <div className="flex text-sm items-center">
+                {isCartURL ? (
+                  <>
+                    <button onClick={() => removeFromCart(product?.id)}>
+                      <CartAction textLabel="Remove" icon={BinIcon} />
+                    </button>
+                    <button onClick={() => handleAddToWishList(product)}>
+                      <CartAction textLabel="Move to list" icon={HeartIcon} />
+                    </button>
+                  </>
+                ) : (
+                  <div className="grid gap-2 grid-cols-2 w-[360px]">
+                    <Button
+                      LoadingState=""
+                      icon={CartIcon}
+                      bg="bg-green-700"
+                      bgHover="bg-green-800"
+                      textLabel="+ Add to Cart"
+                    />
+                    <Button
+                      LoadingState=""
+                      icon={CartIcon}
+                      bg="bg-green-700"
+                      bgHover="bg-green-800"
+                      textLabel="+ Add to Cart"
+                    />
+                  </div>
+                )}
               </div>
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
