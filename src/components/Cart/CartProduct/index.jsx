f7ec2968242f5ec1e924df8feb4580625cf49ab4
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-29 11:50:07 +0200

    src/components/Cart/CartProduct/index.jsx

--- src/components/Cart/CartProduct/index.jsx
+++ src/components/Cart/CartProduct/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { StocksLocations } from "../../Utils/StocksLocations";
 import HeartIcon from "../../../images/Cart/gray-heart.svg";
@@ -13,7 +14,13 @@ export const CartProduct = ({ product }) => {
 
   const isCartURL = location.pathname === "/cart";
 
-  const { removeFromCart, moveWishList, updateCart } = useChecResultContext();
+  const {
+    removeFromCart,
+    moveWishList,
+    updateCart,
+    addToCart,
+    deleteFromWishlist,
+  } = useChecResultContext();
 
   const handleChange = (e) => {
     updateCart(product.id, e.target.value);
@@ -27,25 +34,29 @@ export const CartProduct = ({ product }) => {
   return (
     <div className="h-fit lg:h-[191px] bg-white p-6 border border-gray-200  rounded mb-3 md:mb-6">
       <div className="grid grid-cols-1 sm:grid-cols-[100px_1fr] lg:grid-cols-[140px_1fr] gap-6 h-full">
-        <div className="h-full flex justify-center items-center p-2 border-2 sm:border-none">
-          <img
-            className="w-full h-auto"
-            src={product?.image?.url}
-            alt="cart-product"
-          />
-        </div>
+        <Link to={`/product/${product?.product_id}`}>
+          <div className="h-full flex justify-center items-center p-2 border-2 sm:border-none">
+            <img
+              className="w-full h-auto"
+              src={product?.image?.url}
+              alt="cart-product"
+            />
+          </div>
+        </Link>
         <div className="flex flex-col justify-between">
           <div className="flex justify-between">
             <div>
-              <h2 className="hidden lg:block">
-                {product?.name.slice(0, 60)}...
-              </h2>
-              <h2 className="block lg:hidden">
-                {product?.name.slice(0, 30)}...
-              </h2>
-              <span className="text-xs text-gray-400">
-                Promotion Applied: Massive Sale
-              </span>
+              <Link to={`/product/${product?.product_id}`}>
+                <h2 className="hidden lg:block">
+                  {product?.name.slice(0, 60)}...
+                </h2>
+                <h2 className="block lg:hidden">
+                  {product?.name.slice(0, 30)}...
+                </h2>
+                <span className="text-xs text-gray-400">
+                  Promotion Applied: Massive Sale
+                </span>
+              </Link>
               <div>
                 <StocksLocations />
               </div>
@@ -86,40 +97,41 @@ export const CartProduct = ({ product }) => {
               )}
             </div>
           </div>
-          <div className="flex justify-between">
-            <div className="flex justify-between w-full items-end">
-              <div className="text-xs bg-gray-100 border-2 px-2 py-1 rounded w-fit h-fit border-dashed hidden sm:block">
-                Eligible for Cash on Delivery
-              </div>
-              <div className="flex text-sm items-center">
-                {isCartURL ? (
-                  <>
-                    <button onClick={() => removeFromCart(product?.id)}>
-                      <CartAction textLabel="Remove" icon={BinIcon} />
-                    </button>
-                    <button onClick={() => handleAddToWishList(product)}>
-                      <CartAction textLabel="Move to list" icon={HeartIcon} />
-                    </button>
-                  </>
-                ) : (
-                  <div className="grid gap-2 grid-cols-2 w-[360px]">
+          <div className="grid grid-cols-1 xl:grid-cols-2 gap-3">
+            <div className="text-xs bg-gray-100 border-2 px-2 py-1 rounded w-fit h-fit border-dashed hidden xl:block">
+              Eligible for Cash on Delivery
+            </div>
+            <div className="flex text-sm items-center justify-end">
+              {isCartURL ? (
+                <>
+                  <button onClick={() => removeFromCart(product?.id)}>
+                    <CartAction textLabel="Remove" icon={BinIcon} />
+                  </button>
+                  <button onClick={() => handleAddToWishList(product)}>
+                    <CartAction textLabel="Move to list" icon={HeartIcon} />
+                  </button>
+                </>
+              ) : (
+                <div className="grid gap-2 grid-cols-2 w-[360px]">
+                  <div onClick={() => deleteFromWishlist(product?.product_id)}>
                     <Button
                       LoadingState=""
                       icon={CartIcon}
-                      bg="bg-green-700"
-                      bgHover="bg-green-800"
-                      textLabel="+ Add to Cart"
+                      bg="bg-red-700"
+                      bgHover="bg-red-800"
+                      textLabel="Remove from list"
                     />
+                  </div>
+                  <div onClick={() => addToCart(product?.product_id)}>
                     <Button
-                      LoadingState=""
                       icon={CartIcon}
                       bg="bg-green-700"
                       bgHover="bg-green-800"
                       textLabel="+ Add to Cart"
                     />
                   </div>
-                )}
-              </div>
+                </div>
+              )}
             </div>
           </div>
         </div>
