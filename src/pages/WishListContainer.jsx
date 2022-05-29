b066f0d017920c3c081de745f7784db3e4e4ef97
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-29 11:47:28 +0200

    src/pages/WishListContainer.jsx

--- src/pages/WishListContainer.jsx
+++ src/pages/WishListContainer.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import EmptyIcon from "../images/Wishlist/empty-list.svg";
 import {
   CreateWishList,
   PageTitle,
   Wishlist,
   CartProduct,
+  LoaderSpinner,
+  EmptyItems,
 } from "../components";
+import { useChecResultContext } from "../Context/ChecContextProvider";
 
 export const WishListContainer = () => {
+  const { isWishlistLoading } = useChecResultContext();
+  if (isWishlistLoading) {
+    return (
+      <section className="bg-gray-100">
+        <LoaderSpinner />
+      </section>
+    );
+  }
+
   const wishlistProducts = JSON.parse(localStorage.getItem("wishList"));
+
+  if (wishlistProducts?.length === 0) {
+    return (
+      <EmptyItems
+        pagetitle="Wish List"
+        icon={EmptyIcon}
+        textLabel="Your wish list is empty"
+      />
+    );
+  }
+
   return (
     <section className="bg-gray-100 py-6">
       <div className="container mx-auto">
-        <div className="grid gap-6 grid-cols-1 md:grid-cols-[325px_1fr]">
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-[240px_1fr] lg:grid-cols-[325px_1fr]">
           <CreateWishList />
           <div>
             <PageTitle titeLabel="Wish List" />
