1fd241d36cbf48c87e9f660ebd65341d15f3be45
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-05-31 23:09:41 +0200

    src/components/mainHeader/SideBarCategorySingleLink.jsx

--- src/components/mainHeader/SideBarCategorySingleLink.jsx
+++ src/components/mainHeader/SideBarCategorySingleLink.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import rightIcon from "../../images/right-arrow.svg";
+import { useChecResultContext } from "../../Context/ChecContextProvider";
 
-export const SideBarCategorySingleLink = ({ label, path }) => {
+export const SideBarCategorySingleLink = ({ label }) => {
+  const { queryProduct } = useChecResultContext();
+
   return (
-    <Link key={label} to={path}>
-      <li className="bg-white w-full h-6 hover: hover:bg-blue hover:text-white  items-center px-3 flex justify-between ">
-        <span>{label}</span>
-        <span className="fill-white">
-          <img width="8px" src={rightIcon} alt="" />
-        </span>
-      </li>
-    </Link>
+    <li
+      className="bg-white w-full h-6 hover: hover:bg-blue hover:text-white  items-center px-3 flex justify-between cursor-pointer"
+      onClick={() => queryProduct(label)}
+    >
+      <span>{label}</span>
+      <span className="fill-white">
+        <img width="8px" src={rightIcon} alt="" />
+      </span>
+    </li>
   );
 };
