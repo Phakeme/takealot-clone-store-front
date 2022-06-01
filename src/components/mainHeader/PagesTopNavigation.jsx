d563c1f3dcdf7bcbc2b75b266b92ae9c6a67b5a2
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-01 21:26:56 +0200

    src/components/mainHeader/PagesTopNavigation.jsx

--- src/components/mainHeader/PagesTopNavigation.jsx
+++ src/components/mainHeader/PagesTopNavigation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import BrandLogo from "../../images/takealot-logo.svg";
 import CartIcon from "../../images/shopping-cart.svg";
 import Heart from "../../images/heart.svg";
@@ -8,6 +8,8 @@ import { MyAccounts } from "./MyAccounts";
 import { useChecResultContext } from "../../Context/ChecContextProvider";
 
 export const PagesTopNavigation = () => {
+  const location = useLocation();
+  const currentURL = location.pathname === "/help";
   const {
     cart: { total_items },
   } = useChecResultContext();
@@ -25,36 +27,40 @@ export const PagesTopNavigation = () => {
           />
         </Link>
         <div className="hidden lg:flex items-center divide-x">
-          <PagesTopNavSingleLink path="/login" label="Help" />
-          <PagesTopNavSingleLink path="/login" label="Sell on Takealot" />
+          <PagesTopNavSingleLink path="/help" label="Help" />
+          {!currentURL && (
+            <PagesTopNavSingleLink path="/sell" label="Sell on Takealot" />
+          )}
         </div>
       </div>
-      <div className="flex items-center divide-x">
-        <PagesTopNavSingleLink path="/login" label="Login" />
-        <PagesTopNavSingleLink path="/login" label="Register" />
-        <div className="hidden lg:block">
-          <PagesTopNavSingleLink path="/login" label="Orders" />
-        </div>
-        <div className="text-sm flex items-center justify-center  pl-2 h-8s">
-          <MyAccounts />
-          <Link to="/wishlist">
-            <div className="min-w-8 flex items-center justify-center mx-2 rounded-full bg-red-400 p-2 radius-2 cursor-pointer">
-              <img width="17px" src={Heart} alt="" />
-            </div>
-          </Link>
+      {!currentURL && (
+        <div className="flex items-center divide-x">
+          <PagesTopNavSingleLink path="/login" label="Login" />
+          <PagesTopNavSingleLink path="/login" label="Register" />
+          <div className="hidden lg:block">
+            <PagesTopNavSingleLink path="/login" label="Orders" />
+          </div>
+          <div className="text-sm flex items-center justify-center  pl-2 h-8s">
+            <MyAccounts />
+            <Link to="/wishlist">
+              <div className="min-w-8 flex items-center justify-center mx-2 rounded-full bg-red-400 p-2 radius-2 cursor-pointer">
+                <img width="17px" src={Heart} alt="" />
+              </div>
+            </Link>
 
-          <Link to="/cart">
-            <div className="h-full flex items-center justify-center bg-green-700 rounded-full p-2 text-white cursor-pointer">
-              <div className="">
-                <img width="18px" src={CartIcon} alt="cart" />
+            <Link to="/cart">
+              <div className="h-full flex items-center justify-center bg-green-700 rounded-full p-2 text-white cursor-pointer">
+                <div className="">
+                  <img width="18px" src={CartIcon} alt="cart" />
+                </div>
+                <div className="pl-2 pr-1">
+                  {total_items ? <span>{total_items}</span> : <span>0</span>}
+                </div>
               </div>
-              <div className="pl-2 pr-1">
-                {total_items ? <span>{total_items}</span> : <span>0</span>}
-              </div>
-            </div>
-          </Link>
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
