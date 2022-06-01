c3326d747b7b9d00f39d55d32190d08bfd6d4675
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-01 21:27:20 +0200

    src/components/mainHeader/CategoryTopNavSingleLink.jsx

--- src/components/mainHeader/CategoryTopNavSingleLink.jsx
+++ src/components/mainHeader/CategoryTopNavSingleLink.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useChecResultContext } from "../../Context/ChecContextProvider";
 
 export const CategoryTopNavSingleLink = ({ label }) => {
+  const navigate = useNavigate();
   const { queryProduct } = useChecResultContext();
+
+  const handleSubmit = (textQuery) => {
+    queryProduct(textQuery);
+    navigate("/results", { replace: true });
+  };
 
   return (
     <li
-      className="h-full hover:bg-gray-200 grow"
-      onClick={() => queryProduct(label)}
+      className="h-full hover:bg-gray-200 grow cursor-pointer"
+      onClick={() => handleSubmit(label)}
     >
       <div className="flex items-center justify-center h-full">{label}</div>
     </li>
