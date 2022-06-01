2db64cf4a0b90c7107fcbcd3d38d7dce82766998
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-01 21:24:32 +0200

    src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ import { CartContainer } from "./CartContainer";
 import { WishListContainer } from "./WishListContainer";
 import { MyAccountContainer } from "./MyAccountContainer";
 import { SearchProductsContainer } from "./SearchProductsContainer";
+import { SellContainer } from "./SellContainer";
 
 export {
   HomeContainer,
@@ -16,4 +17,5 @@ export {
   WishListContainer,
   MyAccountContainer,
   SearchProductsContainer,
+  SellContainer,
 };
