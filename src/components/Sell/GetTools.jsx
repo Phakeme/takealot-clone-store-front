517e7827e24e480158c4513e115ce30213609d88
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-02 21:21:44 +0200

    src/components/Sell/GetTools.jsx

---
+++ src/components/Sell/GetTools.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const GetTools = () => {
+  return (
+    <section className="bg-gray-100">
+      <div className="container mx-auto">
+        <div className="text-center pt-10 pb-8">
+          <h2 className="text-[24px] font-bold">
+            Get the tools you need to increase sales and grow your business
+            online
+          </h2>
+          <p>
+            Selling your products online has never been easier. Simply apply to
+            sell as a Takealot seller today and easily reach online shoppers
+            across South Africa.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
