2d32669f835ab05c7266a39c331455dfd7af98de
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-02 21:21:23 +0200

    src/components/Sell/Hero.jsx

---
+++ src/components/Sell/Hero.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import HeroImage from "../../images/sell/codealotbg.jpg";
+
+export const Hero = () => {
+  return (
+    <div
+      className="h-[400px] bg-center flex"
+      style={{
+        backgroundImage: `url(${HeroImage})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="container mx-auto flex items-center">
+        <div
+          className=" text-white p-6 w-[560px]"
+          style={{ background: "rgba(11, 121, 191, 0.9)" }}
+        >
+          <div className="mb-3">
+            <h1 className="text-[48px] font-bold">
+              Join SA's Best Online Portfolio Project
+            </h1>
+            <h2 className="text-[24px]">
+              Sell to over 3 million happy shoppers
+            </h2>
+          </div>
+          <div className="">
+            <a href="#pricing">
+              <div className="px-5 p-3 border w-fit cursor-pointer hover:bg-white hover:text-blue">
+                See Pricing
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
