5a2033e46425353ab27e178ae395b0fb31edd0d4
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-04 00:20:23 +0200

    src/components/Sell/SellSection.jsx

--- src/components/Sell/SellSection.jsx
+++ src/components/Sell/SellSection.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export const SellSection = ({ heading, subHeading, bg, children }) => {
+export const SellSection = ({
+  heading,
+  subHeading,
+  bg,
+  children,
+  textColor,
+}) => {
   return (
-    <section className={`${bg} pb-10`}>
+    <section className={`${bg} pb-10 ${textColor}`}>
       <div className="container mx-auto">
         <div className="text-center pt-[50px] pb-8">
-          <h3 className="text-[28px] font-bold text-black">{heading}</h3>
+          <h3 className={`text-[28px] font-bold ${textColor}`}>{heading}</h3>
           <p className="mb-[40px]">{subHeading}</p>
           {children}
         </div>
