44d027a0fb4bd29d17a3664cbc005192ebf1ebb6
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-06 21:47:44 +0200

    src/components/Products/ProductsPreviews.jsx

--- src/components/Products/ProductsPreviews.jsx
+++ src/components/Products/ProductsPreviews.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useChecResultContext } from "../../Context/ChecContextProvider";
 
-export const ProductsPreviews = ({ labelText, children }) => {
+export const ProductsPreviews = ({ labelText, query, children }) => {
+  let navigate = useNavigate();
+  const { queryProduct } = useChecResultContext();
+  const handleSubmit = (textQuery) => {
+    queryProduct(textQuery);
+    navigate("/results", { replace: true });
+  };
   return (
     <>
       <div
@@ -10,11 +17,14 @@ export const ProductsPreviews = ({ labelText, children }) => {
         className="py-3 flex justify-between items-center text-base lg:text-2xl font-bold mt-3"
       >
         <h2 className="">{labelText}</h2>
-        <Link to="/products">
-          <div className="py-1 px-2 border border-gray-800 rounded hover:bg-gray-800 hover:text-white">
-            <h2 className="text-sm">View More</h2>
-          </div>
-        </Link>
+
+        <div
+          className="py-1 px-2 border border-gray-800 rounded hover:bg-gray-800 hover:text-white cursor-pointer"
+          onClick={() => handleSubmit(query)}
+        >
+          {/* <h2 className="text-sm">View More {query}</h2> */}
+          <h2 className="text-sm">View More</h2>
+        </div>
       </div>
       <div className="" />
       <div className="grid grid-cols-2 md:grid-cols-3 gap-6 lg:h-[280px] xl:h-[315px]">
