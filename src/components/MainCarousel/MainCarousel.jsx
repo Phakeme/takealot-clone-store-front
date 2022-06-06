e778f9974cac2bafcbc5e1409f68e59a853ec133
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-06 21:47:17 +0200

    src/components/MainCarousel/MainCarousel.jsx

--- src/components/MainCarousel/MainCarousel.jsx
+++ src/components/MainCarousel/MainCarousel.jsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// requires a loader
+import { useNavigate } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import carouselBg1 from "../../images/carousel-bg-1.png";
 import carouselBg2 from "../../images/carousel-bg-2.png";
 import carouselBg3 from "../../images/carousel-bg-3.png";
+import carouselBg4 from "../../images/carousel-bg-4.png";
+import { useChecResultContext } from "../../Context/ChecContextProvider";
 
 export const MainCarousel = () => {
+  let navigate = useNavigate();
+  const { queryProduct } = useChecResultContext();
+  const handleSubmit = (textQuery) => {
+    queryProduct(textQuery);
+    navigate("/results", { replace: true });
+  };
   const data = [
     {
-      bgImage: carouselBg1,
-      path: "/product",
+      bgImage: carouselBg4,
+      category: "home",
     },
     {
       bgImage: carouselBg2,
-      path: "/product",
+      category: "sport",
+    },
+    {
+      bgImage: carouselBg1,
+      category: "cooking",
     },
     {
       bgImage: carouselBg3,
-      path: "/product",
+      category: "books",
     },
   ];
   return (
@@ -32,13 +43,17 @@ export const MainCarousel = () => {
       showStatus={false}
       interval={5000}
     >
-      {data?.map(({ path, bgImage }, index) => (
-        <Link to={path} key={index}>
+      {data?.map(({ category, bgImage }, index) => (
+        <div
+          key={index}
+          onClick={() => handleSubmit(category)}
+          className="cursor-pointer"
+        >
           <div
             style={{ backgroundImage: `url(${bgImage})` }}
             className="h-[300px] bg-center"
           />
-        </Link>
+        </div>
       ))}
     </Carousel>
   );
