8bf974ede4015927a9da2e7f09100fe9b7d26c51
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-21 20:22:02 +0200

    Removed navigation on category item, only navigate when the advert is clicked

--- src/components/mainHeader/SideBarCategorySingleLink.jsx
+++ src/components/mainHeader/SideBarCategorySingleLink.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import rightIcon from "../../images/right-arrow.svg";
-import { useChecResultContext } from "../../Context/ChecContextProvider";
 
 export const SideBarCategorySingleLink = ({ label }) => {
-  const navigate = useNavigate();
-  const { queryProduct } = useChecResultContext();
-  const handleSubmit = (textQuery) => {
-    queryProduct(textQuery);
-    navigate("/results", { replace: true });
-  };
-
   return (
-    <div
-      className="bg-white w-full h-6 hover: hover:bg-blue hover:text-white  items-center px-3 flex justify-between cursor-pointer"
-      onClick={() => handleSubmit(label)}
-    >
+    <div className="bg-white w-full h-6 hover: hover:bg-blue hover:text-white  items-center px-3 flex justify-between cursor-pointer">
       <span>{label}</span>
       <span className="fill-white">
         <img width="8px" src={rightIcon} alt="down" />
