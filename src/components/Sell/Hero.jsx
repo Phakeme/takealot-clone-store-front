bc48c947c7f5942fcc52ae31aaed03ae2d571728
Phakeme Fakazi <phakemefakazi@yahoo.com>
2022-06-23 20:52:53 +0200

    Updated to more responsive Sell Page

--- src/components/Sell/Hero.jsx
+++ src/components/Sell/Hero.jsx
@@ -4,7 +4,7 @@ import HeroImage from "../../images/sell/codealotbg.jpg";
 export const Hero = () => {
   return (
     <div
-      className="h-[400px] bg-center flex"
+      className="h-[350px] md:h-[400px] bg-center flex"
       style={{
         backgroundImage: `url(${HeroImage})`,
         backgroundPosition: "center",
@@ -14,14 +14,14 @@ export const Hero = () => {
     >
       <div className="container mx-auto flex items-center">
         <div
-          className=" text-white p-2 md:p-6 w-full md:w-[560px]"
+          className=" text-white p-3 sm:p-8 md:p-6 w-full md:w-[560px]"
           style={{ background: "rgba(11, 121, 191, 0.9)" }}
         >
           <div className="m-1 md:mb-3">
-            <h1 className="text-[28px] md:text-[48px] font-bold">
+            <h1 className="text-[24px] md:text-[28px] md:text-[48px] font-bold">
               Join SA's Best Online Portfolio Project
             </h1>
-            <h2 className="text-[24px]">
+            <h2 className="text-[18px] md:text-[24px]">
               Sell to over 3 million happy shoppers
             </h2>
           </div>
